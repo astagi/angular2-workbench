2d6e5e2031286befd186559591bfd7440b134144
Andrea Stagi <stagi.andrea@gmail.com>
2016-03-02 15:17:35 +0100

    Execute tests in Firefox as well

--- karma.conf.js
+++ karma.conf.js
@@ -40,13 +40,14 @@ module.exports = function(config) {
 
         autoWatch: true,
 
-        browsers: ['Chrome'],
+        browsers: ['Chrome', 'Firefox'],
 
         // Karma plugins loaded
         plugins: [
             'karma-jasmine',
             'karma-coverage',
-            'karma-chrome-launcher'
+            'karma-chrome-launcher',
+            'karma-firefox-launcher'
         ],
 
         // Coverage reporter generates the coverage
